51109f4120692a6739c10bf8250eae2408a21c7e
Riley Chetwood <rileychetwood@gmail.com>
2024-08-14 12:37:40 -0700

    init commit

---
+++ src/main/java/com/riley/practice/spring_in_action/SpringInActionApplication.java
@@ -0,0 +1,14 @@
+package com.riley.practice.spring_in_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInActionApplication.class, args);
+		// wadawd
+	}
+
+}
---
+++ src/test/java/com/riley/practice/spring_in_action/SpringInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riley.practice.spring_in_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
